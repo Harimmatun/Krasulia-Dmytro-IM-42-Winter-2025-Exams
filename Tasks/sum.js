1f84672b61b9988f0af1835c4d003c31456b9d1b
Dmytro Krasulia <harimmatun@gmail.com>
2025-01-13 23:33:36 +0200

    Update sum.js

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -5,8 +5,7 @@ const k = (sum = function (s = () => {}) {
   sum = [0];
   let k = 5;
   for (i of s) {
-    let t = typeof i;
-    if (t === 'number') {
+    if (typeof item === 'number') {
       if (sum.length > 0) {
         const new_Sum = sum[sum.length - 1] + i;
         sum.push(new_Sum);
