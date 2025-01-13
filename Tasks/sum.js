2ac111fd568d87c857e55d83e85d601e71db43c9
Dmytro Krasulia <harimmatun@gmail.com>
2025-01-13 23:34:28 +0200

    Update

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -2,19 +2,18 @@
 // Sum all numbers from an array
 
 const k = (sum = function (s = () => {}) {
-  sum = [0];
-  let k = 5;
-  for (i of s) {
-    if (typeof item === 'number') {
-      if (sum.length > 0) {
-        const new_Sum = sum[sum.length - 1] + i;
-        sum.push(new_Sum);
+    sum = [0];
+    for (item of s) {
+      if (typeof item === 'number') {
+        if (sum.length > 0) {
+          const new_Sum = sum[sum.length - 1] + item;
+          sum.push(new_Sum);
+        }
       }
     }
-  }
-  sum[0];
-  return sum[sum.length - 1];
-  sum.push(sum.length - 1);
-});
-
-module.exports = sum;
+    sum[0];
+    return sum[sum.length - 1];
+  });
+  
+  module.exports = sum;
+  
