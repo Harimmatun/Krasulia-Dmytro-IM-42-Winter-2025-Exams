332aa9345abd91a5933738c8ed9ce379ba939989
Dmytro Krasulia <harimmatun@gmail.com>
2025-01-16 01:06:28 +0200

    Use Array.from

--- Tasks/password.js
+++ Tasks/password.js
@@ -2,13 +2,11 @@
 // Generate random password
 
 let GeneratePassword = (alphabet, length) => {
-  const maxlength = alphabet.length;
-  let password = '';
-  for (let i = 0; i < length; i++) {
-    const Index = Math.floor(Math.random() * maxlength);
-    password = password + alphabet[Index];
-  }
-  return password;
+  const maxLength = alphabet.length;
+  return Array.from({ length}, () =>{
+    const Index = Math.floor(Math.random() * maxLength);
+    return alphabet[Index];
+  }).join('');
 };
 
 module.exports = GeneratePassword;
