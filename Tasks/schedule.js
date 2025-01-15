55c0b2cf69efa91ffa79d6cb6dca8b391f9c4cf7
Dmytro Krasulia <harimmatun@gmail.com>
2025-01-15 17:12:51 +0200

    Add interval and finish this task

--- Tasks/schedule.js
+++ Tasks/schedule.js
@@ -6,6 +6,7 @@
 const schedule = async (func, n, interval) => {
     for (let i = 0; i < n; i++){
         func(i);
+        await new Promise((resolve) => setTimeout(resolve, interval));
     }
 };
 
