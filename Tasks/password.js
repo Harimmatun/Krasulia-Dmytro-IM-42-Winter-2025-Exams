78ed54bd8bb1b87a8fa618917b058e2a98831c59
Dmytro Krasulia <harimmatun@gmail.com>
2025-01-16 00:59:12 +0200

    Update some variables

--- Tasks/password.js
+++ Tasks/password.js
@@ -3,9 +3,9 @@
 
 let GeneratePassword = (alphabet, length) => {
   const MAX = alphabet.length;
-  key = '';
+  let key = '';
   for (let i = 0; i < length; i++) {
-    Index = Math.floor(Math.random() * MAX);
+    let Index = Math.floor(Math.random() * MAX);
     key = key + alphabet[Index];
   }
   return key;
